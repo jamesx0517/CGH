f381f9ba291115fe1e32306fe80614fb953ec9ce
jamesx0517 <jamesx0517@gmail.com>
2019-03-22 22:38:07 +0800

    修改getid

--- CGH/App_Start/RouteConfig.cs
+++ CGH/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace CGH
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{_ID}",
+                defaults: new { controller = "CGH", action = "Index", _ID = UrlParameter.Optional }
             );
         }
     }
