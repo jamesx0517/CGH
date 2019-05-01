170598b1783a1978b81815cef6afc36352e7487b
jamesx0517 <jamesx0517@gmail.com>
2019-05-01 11:37:58 +0800

    印花稅
    印花稅

--- CGH/helper/DecimalPoint.cs
+++ CGH/helper/DecimalPoint.cs
@@ -12,5 +12,18 @@ namespace CGH.helper
         {
             return value / 1.000000000000000000000000000000000m;
         }
+
+        public static decimal Tax(this HtmlHelper helper, decimal value)
+        { if (value >= 250)
+            { decimal tax = value / 250;
+
+                return Math.Floor(tax); 
+            }
+            else { decimal tax = 0;
+
+                return tax;
+            }
+            
+        }
     }
 }
